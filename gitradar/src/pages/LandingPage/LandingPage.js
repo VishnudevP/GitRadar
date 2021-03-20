b846519a83fa09865ce5085445c74f628afc1e01
VishnudevP <62526318+VishnudevP@users.noreply.github.com>
2021-03-19 22:39:34 -0400

    Landing Content

--- gitradar/src/pages/LandingPage/LandingPage.js
+++ gitradar/src/pages/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import LandingContent from './components/LandingContent/LandingContent'
 import NavBar from './components/NavBar/NavBar'
 
 const LandingPage = () => {
@@ -6,6 +7,7 @@ const LandingPage = () => {
 
         <div>
             <NavBar/>
+            <LandingContent main={["As much as we love it, sometimes", <br/>, "version control", <span> can be a pain </span>]} sub="That’s why GitRadar makes it easy to get notified about events your version control system. With GitRadar, you can get notifications when your teammate commits and when your latest release is deployed, straight to Discord."  />
         </div>
         
     );
